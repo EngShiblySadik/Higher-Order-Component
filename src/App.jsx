c379b45ee61ec0e5acc706a1ab8d7aa0a5d1f951
Md Shibly Sadik <118973538+EngShiblySadik@users.noreply.github.com>
2023-06-14 11:42:26 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import UpdateComponent from "./hoc/main";
+
+function App(props) {
+  return (
+    <>
+      <div>
+        <h1>Count: {props.count.count}</h1>
+        <br />
+        <button onClick={props.increment}>Increment</button>
+        <button onClick={props.decrement}>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default UpdateComponent(App);
